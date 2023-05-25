c6c2555ece6db8e26e1cd78eda18434187a8d6e2
Ben Greenman <benjaminlgreenman@gmail.com>
2023-05-24 22:25:59 -0400

    float, shallow runs in SP yay

--- Benchmark/float/shallow.py
+++ Benchmark/float/shallow.py
@@ -1,8 +1,6 @@
-from typing import List
 from __future__ import annotations
-from __static__ import fields
+from typing import List
 from math import sin, cos, sqrt
-from Timer import Timer
 
 """
 bg:
@@ -13,30 +11,13 @@ bg:
 - remove unused imports
 """
 
-from math import sin, cos, sqrt
-
-
-@fields({'x': float, 'y': float, 'z': float})
 class Point(object):
 
-    # def __init__(self: Point, i: float) -> Void:
-    #     self.x = x = sin(i)
-    #     self.y = cos(i) * 3
-    #     self.z = (x * x) / 2
-
     def __init__(self: Point, i: float) -> None:
-        self.x: float = x = sin(i)  # does this not have to be a float?
+        x: float = sin(i)  # does this not have to be a float?
+        self.x: float = x
         self.y: float = cos(i) * 3
         self.z: float = (x * x) / 2
-
-    # def normalize(self: Point) -> Void:
-    #     x = self.x
-    #     y = self.y
-    #     z = self.z
-    #     norm = sqrt(x * x + y * y + z * z)
-    #     self.x /= norm
-    #     self.y /= norm
-    #     self.z /= norm
 
     def normalize(self: Point) -> None:
         x: float = self.x
@@ -54,22 +35,12 @@ class Point(object):
         return self
 
 
-# def maximize(points: List(Point)) -> Point:
-#     next = points[0]
-#     for p in points[1:]:
-#         next = next.maximize(p)
-#     return next
 def maximize(points: List[Point]) -> Point:
     next: Point = points[0]
     for p in points[1:]:
         next = next.maximize(p)
     return next
 
-# def benchmark(n: int) -> Point:
-#     points = [Point(i) for i in range(n)]
-#     for p in points:
-#         p.normalize()
-#     return maximize(points)
 def benchmark(n: int) -> Point:
     points: List[Point] = [Point(i) for i in range(n)]
     for p in points:
@@ -79,6 +50,4 @@ def benchmark(n: int) -> Point:
 POINTS = 200000
 
 if __name__ == "__main__":
-    t = Timer()
-    with t:
-        benchmark(POINTS)
+    benchmark(POINTS)
