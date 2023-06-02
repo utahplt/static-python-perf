0a5707bd972ba691470577114ba524bcc0c585cc
Vivaan <vivaan.rajesh2006@gmail.com>
2023-06-02 17:00:23 -0600

    Pidigits. Please check

---
+++ Benchmark/pidigits/shallow/main.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+import __static__
+from typing import List, Tuple
+
+"""
+Calculating (some of) the digits of pi. This stresses big integer
+arithmetic.
+"""
+
+"""
+bg
+- remove toplevel function, add `ITERATIONS` constant
+- replaced iterators with lists
+- increased NDIGITS to `5000`
+"""
+
+NDIGITS: int = 5000
+
+
+# Adapted from code on http://shootout.alioth.debian.org/
+def gen_x(k: int) -> Tuple[int, int, int, int]:
+    return (k, 4 * k + 2, 0, 2 * k + 1)
+
+
+def compose(a: Tuple[int, int, int, int], b: Tuple[int, int, int, int]) -> Tuple[int, int, int, int]:
+    aq, ar, as_, at = a
+    bq, br, bs, bt = b
+    return (aq * bq,
+            aq * br + ar * bt,
+            as_ * bq + at * bs,
+            as_ * br + at * bt)
+
+
+def extract(z: Tuple[int, int, int, int], j: int) -> int:
+    q, r, s, t = z
+    return (q * j + r) // (s * j + t)
+
+
+def pi_digits(limit: int) -> List[int]:
+    z = (1, 0, 0, 1)
+    x = 1
+    result: List[int] = []
+    while x <= limit:
+        y = extract(z, 3)
+        while y != extract(z, 4):
+            z = compose(z, gen_x(x))
+            y = extract(z, 3)
+        z = compose((10, -10 * y, 0, 1), z)
+        x += 1
+        result.append(y)
+    return result
+
+
+def calc_ndigits(n: int) -> List[int]:
+    return pi_digits(n)
+
+
+if __name__ == "__main__":
+    calc_ndigits(NDIGITS)
---
+++ Benchmark/pidigits/untyped/main.py
@@ -0,0 +1,58 @@
+"""
+Calculating (some of) the digits of pi. This stresses big integer
+arithmetic.
+"""
+
+"""
+bg
+- remove toplevel function, add `ITERATIONS` constant
+- replaced iterators with lists
+- increased NDIGITS to `5000`
+"""
+
+# import itertools
+
+NDIGITS = 5000
+
+
+def gen_x(k):
+    return (k, 4 * k + 2, 0, 2 * k + 1)
+
+
+def compose(a, b):
+    aq, ar, as_, at = a
+    bq, br, bs, bt = b
+    return (aq * bq,
+            aq * br + ar * bt,
+            as_ * bq + at * bs,
+            as_ * br + at * bt)
+
+
+def extract(z, j):
+    q, r, s, t = z
+    return (q * j + r) // (s * j + t)
+
+
+def pi_digits(limit):
+    z = (1, 0, 0, 1)
+    x = 1
+    result = []
+    while x <= limit:
+        y = extract(z, 3)
+        while y != extract(z, 4):
+            z = compose(z, gen_x(x))
+            y = extract(z, 3)
+        z = compose((10, -10 * y, 0, 1), z)
+        x += 1
+        result.append(y)
+    return result
+
+
+def calc_ndigits(n):
+    return pi_digits(n)
+
+
+if __name__ == "__main__":
+    # print (calc_ndigits(NDIGITS)) testing here
+    calc_ndigits(NDIGITS)
+
