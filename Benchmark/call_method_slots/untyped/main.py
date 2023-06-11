0db5b6512bd557cb487c7bb387bc41664ac13def
Vivaan <vivaan.rajesh2006@gmail.com>
2023-06-11 00:27:41 -0600

    call method slots

---
+++ Benchmark/call_method_slots/untyped/main.py
@@ -0,0 +1,141 @@
+"""Microbenchmark for method call overhead on types that define __slots__.
+
+This measures simple method calls for objects with no dicts that are
+predictable, do not use varargs or kwargs, and do not use tuple unpacking.
+When an object has no __dict__ attribute, the JIT can optimize away most of the
+attribute lookup.  This benchmark measures how well it can do that.
+
+bg:
+- annotated all parameters + return types
+- using Timer
+- removed command-line parsing
+"""
+
+
+class Foo(object):
+    __slots__ = ()
+
+    def foo(self, a, b, c, d):
+        # 20 calls
+        self.bar(a, b, c)
+        self.bar(a, b, c)
+        self.bar(a, b, c)
+        self.bar(a, b, c)
+        self.bar(a, b, c)
+        self.bar(a, b, c)
+        self.bar(a, b, c)
+        self.bar(a, b, c)
+        self.bar(a, b, c)
+        self.bar(a, b, c)
+        self.bar(a, b, c)
+        self.bar(a, b, c)
+        self.bar(a, b, c)
+        self.bar(a, b, c)
+        self.bar(a, b, c)
+        self.bar(a, b, c)
+        self.bar(a, b, c)
+        self.bar(a, b, c)
+        self.bar(a, b, c)
+        self.bar(a, b, c)
+
+    def bar(self, a, b, c):
+        # 20 calls
+        self.baz(a, b)
+        self.baz(a, b)
+        self.baz(a, b)
+        self.baz(a, b)
+        self.baz(a, b)
+        self.baz(a, b)
+        self.baz(a, b)
+        self.baz(a, b)
+        self.baz(a, b)
+        self.baz(a, b)
+        self.baz(a, b)
+        self.baz(a, b)
+        self.baz(a, b)
+        self.baz(a, b)
+        self.baz(a, b)
+        self.baz(a, b)
+        self.baz(a, b)
+        self.baz(a, b)
+        self.baz(a, b)
+        self.baz(a, b)
+
+    def baz(self, a, b):
+        # 20 calls
+        self.quux(a)
+        self.quux(a)
+        self.quux(a)
+        self.quux(a)
+        self.quux(a)
+        self.quux(a)
+        self.quux(a)
+        self.quux(a)
+        self.quux(a)
+        self.quux(a)
+        self.quux(a)
+        self.quux(a)
+        self.quux(a)
+        self.quux(a)
+        self.quux(a)
+        self.quux(a)
+        self.quux(a)
+        self.quux(a)
+        self.quux(a)
+        self.quux(a)
+
+    def quux(self, a):
+        # 20 calls
+        self.qux()
+        self.qux()
+        self.qux()
+        self.qux()
+        self.qux()
+        self.qux()
+        self.qux()
+        self.qux()
+        self.qux()
+        self.qux()
+        self.qux()
+        self.qux()
+        self.qux()
+        self.qux()
+        self.qux()
+        self.qux()
+        self.qux()
+        self.qux()
+        self.qux()
+        self.qux()
+
+    def qux(self):
+        pass
+
+
+def test_calls():
+    f = Foo()
+    # 20 calls
+    f.foo(1, 2, 3, 4)
+    f.foo(1, 2, 3, 4)
+    f.foo(1, 2, 3, 4)
+    f.foo(1, 2, 3, 4)
+    f.foo(1, 2, 3, 4)
+    f.foo(1, 2, 3, 4)
+    f.foo(1, 2, 3, 4)
+    f.foo(1, 2, 3, 4)
+    f.foo(1, 2, 3, 4)
+    f.foo(1, 2, 3, 4)
+    f.foo(1, 2, 3, 4)
+    f.foo(1, 2, 3, 4)
+    f.foo(1, 2, 3, 4)
+    f.foo(1, 2, 3, 4)
+    f.foo(1, 2, 3, 4)
+    f.foo(1, 2, 3, 4)
+    f.foo(1, 2, 3, 4)
+    f.foo(1, 2, 3, 4)
+    f.foo(1, 2, 3, 4)
+    f.foo(1, 2, 3, 4)
+    return
+
+
+if __name__ == "__main__":
+    print(test_calls())
