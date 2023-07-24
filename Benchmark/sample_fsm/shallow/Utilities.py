5b90fcac2b12b62d80b930bf71c4d8b3acee71d6
Vivaan Rajesh <63882442+vivaan2006@users.noreply.github.com>
2023-07-23 22:41:19 -0600

    Update Utilities.py

--- Benchmark/sample_fsm/shallow/Utilities.py
+++ Benchmark/sample_fsm/shallow/Utilities.py
@@ -1,10 +1,11 @@
 import os, itertools
 import __static__
+from typing import List
 fname = "util-random-numbers.txt"
 #TODO: Cannot type variable in retic
 rand_num = itertools.cycle((float(line.strip()) for line in open(fname, "r")))
 
-def accumulated_s(probabilities:List(Float))->List(Float):
+def accumulated_s(probabilities:List[float])->List[float]:
     total = sum(probabilities)
     payoffs = probabilities
     result = []
@@ -14,7 +15,7 @@ def accumulated_s(probabilities:List(Float))->List(Float):
         result = result + [next/total]
     return result
 
-def choose_randomly(probabilities:List(Float), speed:Int)->List(Int):
+def choose_randomly(probabilities:List[float], speed:int)->List[int]:
 
     s = accumulated_s(probabilities)
     res = []  ### changed here
@@ -27,5 +28,5 @@ def choose_randomly(probabilities:List(Float), speed:Int)->List(Int):
                 break
     return res  ### and here
 
-def relative_average(l: List(Float), w: Float) -> Float:
+def relative_average(l: List[float], w: float) -> float:
     return sum(l) / w / len(l)
