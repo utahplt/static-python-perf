9d9daf1ec7ee149b92e1dd522a159cc45a4a1a6c
Vivaan Rajesh <63882442+vivaan2006@users.noreply.github.com>
2023-07-24 19:33:02 -0600

    all typed

--- Benchmark/stats/shallow/inferential.py
+++ Benchmark/stats/shallow/inferential.py
@@ -4,12 +4,15 @@ import probability
 import variability
 import support
 from typed_math import pow, sqrt, exp, abs, fabs, log, round, pi
+from typing import List
+import __static__
+
 
 ####################################
 #####  INFERENTIAL STATISTICS  #####
 ####################################
 
-def ttest_1samp(a:List(float),popmean:int)->(float,float):
+def ttest_1samp(a: List[float], popmean: int) -> (float, float):
     """
 Calculates the t-obtained for the independent samples T-test on ONE group
 of scores a, given a population mean.  If printit=1, results are printed
@@ -19,27 +22,27 @@ using the given writemode (default=append).  Returns t-value, and prob.
 Usage:   lttest_1samp(a,popmean,Name='Sample',printit=0,writemode='a')
 Returns: t-value, two-tailed prob
 """
-    printit=0     #bg: optional arg
-    name='Sample' #bg: optional arg
-    writemode='a' #bg: optional arg
+    printit = 0  # bg: optional arg
+    name = 'Sample'  # bg: optional arg
+    writemode = 'a'  # bg: optional arg
     x = central_tendency.mean(a)
     v = variability.var(a)
     n = len(a)
-    df = n-1
-    svar = ((n-1)*v)/float(df)
-    t = (x-popmean)/sqrt(svar*(1.0/n))
-    prob = probability.betai(0.5*df,0.5,float(df)/(df+t*t))
+    df = n - 1
+    svar = ((n - 1) * v) / float(df)
+    t = (x - popmean) / sqrt(svar * (1.0 / n))
+    prob = probability.betai(0.5 * df, 0.5, float(df) / (df + t * t))
 
     if printit != 0:
         statname = 'Single-sample T-test.'
-        outputpairedstats(printit,writemode,
-                          'Population','--',popmean,0,0,0,
-                          name,n,x,v,min(a),max(a),
-                          statname,t,prob)
-    return t,prob
+        outputpairedstats(printit, writemode,
+                          'Population', '--', popmean, 0, 0, 0,
+                          name, n, x, v, min(a), max(a),
+                          statname, t, prob)
+    return t, prob
 
 
-def ttest_ind (a:List(float), b:List(float))->(float,float):
+def ttest_ind(a: List[float], b: List[float]) -> (float, float):
     """
 Calculates the t-obtained T-test on TWO INDEPENDENT samples of
 scores a, and b.  From Numerical Recipies, p.483.  If printit=1, results
@@ -50,28 +53,31 @@ and prob.
 Usage:   lttest_ind(a,b,printit=0,name1='Samp1',name2='Samp2',writemode='a')
 Returns: t-value, two-tailed prob
 """
-    printit=0; name1='Samp1'; name2='Samp2'; writemode='a'; #bg: optional args
+    printit = 0;
+    name1 = 'Samp1';
+    name2 = 'Samp2';
+    writemode = 'a';  # bg: optional args
     x1 = central_tendency.mean(a)
     x2 = central_tendency.mean(b)
-    v1 = variability.stdev(a)**2
-    v2 = variability.stdev(b)**2
+    v1 = variability.stdev(a) ** 2
+    v2 = variability.stdev(b) ** 2
     n1 = len(a)
     n2 = len(b)
-    df = n1+n2-2
-    svar = ((n1-1)*v1+(n2-1)*v2)/float(df)
-    t = (x1-x2)/sqrt(svar*(1.0/n1 + 1.0/n2))
-    prob = probability.betai(0.5*df,0.5,df/(df+t*t))
+    df = n1 + n2 - 2
+    svar = ((n1 - 1) * v1 + (n2 - 1) * v2) / float(df)
+    t = (x1 - x2) / sqrt(svar * (1.0 / n1 + 1.0 / n2))
+    prob = probability.betai(0.5 * df, 0.5, df / (df + t * t))
 
     if printit != 0:
         statname = 'Independent samples T-test.'
-        outputpairedstats(printit,writemode,
-                          name1,n1,x1,v1,min(a),max(a),
-                          name2,n2,x2,v2,min(b),max(b),
-                          statname,t,prob)
-    return t,prob
+        outputpairedstats(printit, writemode,
+                          name1, n1, x1, v1, min(a), max(a),
+                          name2, n2, x2, v2, min(b), max(b),
+                          statname, t, prob)
+    return t, prob
 
 
-def ttest_rel (a:List(float),b:List(float))->(float,float):
+def ttest_rel(a: List[float], b: List[float]) -> (float, float):
     """
 Calculates the t-obtained T-test on TWO RELATED samples of scores,
 a and b.  From Numerical Recipies, p.483.  If printit=1, results are
@@ -82,8 +88,11 @@ and prob.
 Usage:   lttest_rel(a,b,printit=0,name1='Sample1',name2='Sample2',writemode='a')
 Returns: t-value, two-tailed prob
 """
-    printit=0;name1='Sample1';name2='Sample2';writemode='a' #bg: optional arg
-    if len(a)!=len(b):
+    printit = 0;
+    name1 = 'Sample1';
+    name2 = 'Sample2';
+    writemode = 'a'  # bg: optional arg
+    if len(a) != len(b):
         raise ValueError('Unequal length lists in ttest_rel.')
     x1 = central_tendency.mean(a)
     x2 = central_tendency.mean(b)
@@ -92,22 +101,23 @@ Returns: t-value, two-tailed prob
     n = len(a)
     cov = 0
     for i in range(len(a)):
-        cov = cov + (a[i]-x1) * (b[i]-x2)
-    df = n-1
+        cov = cov + (a[i] - x1) * (b[i] - x2)
+    df = n - 1
     _cov = cov / float(df)
-    sd = sqrt((v1+v2 - 2.0*_cov)/float(n))
-    t = (x1-x2)/sd
-    prob = probability.betai(0.5*df,0.5,df/(df+t*t))
+    sd = sqrt((v1 + v2 - 2.0 * _cov) / float(n))
+    t = (x1 - x2) / sd
+    prob = probability.betai(0.5 * df, 0.5, df / (df + t * t))
 
     if printit != 0:
         statname = 'Related samples T-test.'
-        outputpairedstats(printit,writemode,
-                          name1,n,x1,v1,min(a),max(a),
-                          name2,n,x2,v2,min(b),max(b),
-                          statname,t,prob)
+        outputpairedstats(printit, writemode,
+                          name1, n, x1, v1, min(a), max(a),
+                          name2, n, x2, v2, min(b), max(b),
+                          statname, t, prob)
     return t, prob
 
-def chisquare(f_obs:List(float))->(float,float):
+
+def chisquare(f_obs: List[float]) -> (float, float):
     """
 Calculates a one-way chi square for list of observed frequencies and returns
 the result.  If no expected frequencies are given, the total N is assumed to
@@ -116,17 +126,17 @@ be equally distributed across all groups.
 Usage:   lchisquare(f_obs, f_exp=None)   f_obs = list of observed cell freq.
 Returns: chisquare-statistic, associated p-value
 """
-    f_exp=None #bg: optional arg
-    k = len(f_obs)                 # number of groups
+    f_exp = None  # bg: optional arg
+    k = len(f_obs)  # number of groups
     if f_exp == None:
-        f_exp = [sum(f_obs)/float(k)] * len(f_obs) # create k bins with = freq.
+        f_exp = [sum(f_obs) / float(k)] * len(f_obs)  # create k bins with = freq.
     chisq = 0
     for i in range(len(f_obs)):
-        chisq = chisq + (f_obs[i]-f_exp[i])**2 / float(f_exp[i])
-    return chisq, probability.chisqprob(chisq, k-1)
+        chisq = chisq + (f_obs[i] - f_exp[i]) ** 2 / float(f_exp[i])
+    return chisq, probability.chisqprob(chisq, k - 1)
 
 
-def ks_2samp (data1:List(float),data2:List(float))->(float,float):
+def ks_2samp(data1: List[float], data2: List[float]) -> (float, float):
     """
 Computes the Kolmogorov-Smirnof statistic on 2 samples.  From
 Numerical Recipies in C, page 493.
@@ -146,26 +156,26 @@ Returns: KS D-value, associated p-value
     data1.sort()
     data2.sort()
     while j1 < n1 and j2 < n2:
-        d1=data1[j1]
-        d2=data2[j2]
+        d1 = data1[j1]
+        d2 = data2[j2]
         if d1 <= d2:
-            fn1 = (j1)/float(en1)
+            fn1 = (j1) / float(en1)
             j1 = j1 + 1
         if d2 <= d1:
-            fn2 = (j2)/float(en2)
+            fn2 = (j2) / float(en2)
             j2 = j2 + 1
-        dt = (fn2-fn1)
+        dt = (fn2 - fn1)
         if fabs(dt) > fabs(d):
             d = dt
     try:
-        en = sqrt(en1*en2/float(en1+en2))
-        prob = ksprob((en+0.12+0.11/en)*abs(d))
+        en = sqrt(en1 * en2 / float(en1 + en2))
+        prob = ksprob((en + 0.12 + 0.11 / en) * abs(d))
     except:
         prob = 1.0
     return d, prob
 
 
-def mannwhitneyu(x:List(float),y:List(float))->(float,float):
+def mannwhitneyu(x: List[float], y: List[float]) -> (float, float):
     """
 Calculates a Mann-Whitney U statistic on the provided scores and
 returns the result.  Use only when the n in each condition is < 20 and
@@ -179,23 +189,23 @@ Returns: u-statistic, one-tailed p-value (i.e., p(z(U)))
 """
     n1 = len(x)
     n2 = len(y)
-    ranked = support.rankdata(x+y)
-    rankx = ranked[0:n1]       # get the x-ranks
-    ranky = ranked[n1:]        # the rest are y-ranks
-    u1 = n1*n2 + (n1*(n1+1))/2.0 - sum(rankx)  # calc U for x
-    u2 = n1*n2 - u1                            # remainder is U for y
-    bigu = max(u1,u2)
-    smallu = min(u1,u2)
-    proportion = bigu/float(n1*n2)
+    ranked = support.rankdata(x + y)
+    rankx = ranked[0:n1]  # get the x-ranks
+    ranky = ranked[n1:]  # the rest are y-ranks
+    u1 = n1 * n2 + (n1 * (n1 + 1)) / 2.0 - sum(rankx)  # calc U for x
+    u2 = n1 * n2 - u1  # remainder is U for y
+    bigu = max(u1, u2)
+    smallu = min(u1, u2)
+    proportion = bigu / float(n1 * n2)
     T = sqrt(tiecorrect(ranked))  # correction factor for tied scores
     if T == 0:
         raise ValueError('All numbers are identical in lmannwhitneyu')
-    sd = sqrt(T*n1*n2*(n1+n2+1)/12.0)
-    z = abs((bigu-n1*n2/2.0) / sd)  # normal approximation for prob calc
-    return smallu, 1.0 - probability.zprob(z) #, proportion
+    sd = sqrt(T * n1 * n2 * (n1 + n2 + 1) / 12.0)
+    z = abs((bigu - n1 * n2 / 2.0) / sd)  # normal approximation for prob calc
+    return smallu, 1.0 - probability.zprob(z)  # , proportion
 
 
-def tiecorrect(rankvals:List(float))->float:
+def tiecorrect(rankvals: List[float]) -> float:
     """
 Corrects for ties in Mann Whitney U and Kruskal Wallis H tests.  See
 Siegel, S. (1956) Nonparametric Statistics for the Behavioral Sciences.
@@ -204,23 +214,23 @@ New York: McGraw-Hill.  Code adapted from |Stat rankind.c code.
 Usage:   ltiecorrect(rankvals)
 Returns: T correction factor for U or H
 """
-    sorted,posn = support.shellsort(rankvals)
+    sorted, posn = support.shellsort(rankvals)
     n = len(sorted)
     T = 0.0
     i = 0
-    while (i<n-1):
-        if sorted[i] == sorted[i+1]:
+    while (i < n - 1):
+        if sorted[i] == sorted[i + 1]:
             nties = 1
-            while (i<n-1) and (sorted[i] == sorted[i+1]):
-                nties = nties +1
-                i = i +1
-            T = T + nties**3 - nties
-        i = i+1
-    T = T / float(n**3-n)
+            while (i < n - 1) and (sorted[i] == sorted[i + 1]):
+                nties = nties + 1
+                i = i + 1
+            T = T + nties ** 3 - nties
+        i = i + 1
+    T = T / float(n ** 3 - n)
     return 1.0 - T
 
 
-def ranksums(x:List(float), y:List(float))->(float,float):
+def ranksums(x: List[float], y: List[float]) -> (float, float):
     """
 Calculates the rank sums statistic on the provided scores and
 returns the result.  Use only when the n in each condition is > 20 and you
@@ -231,18 +241,18 @@ Returns: a z-statistic, two-tailed p-value
 """
     n1 = len(x)
     n2 = len(y)
-    alldata = x+y
+    alldata = x + y
     ranked = support.rankdata(alldata)
     x = ranked[:n1]
     y = ranked[n1:]
     s = sum(x)
-    expected = n1*(n1+n2+1) / 2.0
-    _z = (s - expected) / sqrt(n1*n2*(n1+n2+1)/12.0)
-    prob = 2*(1.0 -probability.zprob(abs(_z)))
+    expected = n1 * (n1 + n2 + 1) / 2.0
+    _z = (s - expected) / sqrt(n1 * n2 * (n1 + n2 + 1) / 12.0)
+    prob = 2 * (1.0 - probability.zprob(abs(_z)))
     return _z, prob
 
 
-def wilcoxont(x:List(float), y:List(float))->(float,float):
+def wilcoxont(x: List[float], y: List[float]) -> (float, float):
     """
 Calculates the Wilcoxon T-test for related samples and returns the
 result.  A non-parametric T-test.
@@ -252,13 +262,13 @@ Returns: a t-statistic, two-tail probability estimate
 """
     if len(x) != len(y):
         raise ValueError('Unequal N in wilcoxont.  Aborting.')
-    d=[]
+    d = []
     for i in range(len(x)):
         diff = x[i] - y[i]
         if diff != 0:
             d.append(diff)
     count = len(d)
-    absd = list(map(abs,d))
+    absd = list(map(abs, d))
     absranked = support.rankdata(absd)
     r_plus = 0.0
     r_minus = 0.0
@@ -268,14 +278,14 @@ Returns: a t-statistic, two-tail probability estimate
         else:
             r_plus = r_plus + absranked[i]
     wt = min(r_plus, r_minus)
-    mn = count * (count+1) * 0.25
-    se =  sqrt(count*(count+1)*(2.0*count+1.0)/24.0)
-    _z = fabs(wt-mn) / se
-    prob = 2*(1.0 -probability.zprob(abs(_z)))
+    mn = count * (count + 1) * 0.25
+    se = sqrt(count * (count + 1) * (2.0 * count + 1.0) / 24.0)
+    _z = fabs(wt - mn) / se
+    prob = 2 * (1.0 - probability.zprob(abs(_z)))
     return wt, prob
 
 
-def kruskalwallish(args:List(List(float)))->(float,float):
+def kruskalwallish(args: List[List[float]]) -> (float, float):
     """
 The Kruskal-Wallis H-test is a non-parametric ANOVA for 3 or more
 groups, requiring at least 5 subjects in each group.  This function
@@ -286,9 +296,9 @@ Usage:   lkruskalwallish(*args)
 Returns: H-statistic (corrected for ties), associated p-value
 """
     args = list(args)
-    n = [0]*len(args)
+    n = [0] * len(args)
     all = []
-    n = list(map(len,args))
+    n = list(map(len, args))
     for i in range(len(args)):
         all = all + args[i]
     ranked = support.rankdata(all)
@@ -298,18 +308,19 @@ Returns: H-statistic (corrected for ties), associated p-value
         del ranked[0:n[i]]
     rsums = []
     for i in range(len(args)):
-        rsums.append(sum(args[i])**2)
+        rsums.append(sum(args[i]) ** 2)
         rsums[i] = rsums[i] / float(n[i])
     ssbn = sum(rsums)
     totaln = sum(n)
-    h = 12.0 / (totaln*(totaln+1)) * ssbn - 3*(totaln+1)
+    h = 12.0 / (totaln * (totaln + 1)) * ssbn - 3 * (totaln + 1)
     df = len(args) - 1
     if T == 0:
         raise ValueError('All numbers are identical in lkruskalwallish')
     h = h / float(T)
-    return h, probability.chisqprob(h,df)
+    return h, probability.chisqprob(h, df)
 
-def friedmanchisquare(args:List(List(float)))->(float,float):
+
+def friedmanchisquare(args: List[List[float]]) -> (float, float):
     """
 Friedman Chi-Square is a non-parametric, one-way within-subjects
 ANOVA.  This function calculates the Friedman Chi-square test for repeated
@@ -327,13 +338,12 @@ Returns: chi-square statistic, associated p-value
     if k > 3:
         raise ValueError('bg: specialized this code to 3 levels for Reticulated experiment')
     n = len(args[0])
-    #bg#data = pstat.abut(l0,l1,l2)
-    data = pstat.abut(pstat.abut(args[0], args[1]),args[2])
+    # bg#data = pstat.abut(l0,l1,l2)
+    data = pstat.abut(pstat.abut(args[0], args[1]), args[2])
     for i in range(len(data)):
         data[i] = support.rankdata(data[i])
     ssbn = 0
     for i in range(k):
-        ssbn = ssbn + sum(args[i])**2
-    chisq = 12.0 / (k*n*(k+1)) * ssbn - 3*n*(k+1)
-    return chisq, probability.chisqprob(chisq,k-1)
-
+        ssbn = ssbn + sum(args[i]) ** 2
+    chisq = 12.0 / (k * n * (k + 1)) * ssbn - 3 * n * (k + 1)
+    return chisq, probability.chisqprob(chisq, k - 1)
--- Benchmark/stats/shallow/moment.py
+++ Benchmark/stats/shallow/moment.py
@@ -1,12 +1,15 @@
 from typed_math import pow, sqrt, exp, abs, fabs, log, round, pi
 import central_tendency
 import variability
+import __static__
+from typing import List
+
 
 ####################################
 ############  MOMENTS  #############
 ####################################
 
-def moment(inlist:List(float),moment:int)->float:
+def moment(inlist: List[float], moment: int) -> float:
     """
 Calculates the nth moment about the mean for a sample (defaults to
 the 1st moment).  Used to calculate coefficients of skewness and kurtosis.
@@ -21,41 +24,41 @@ Returns: appropriate moment (r) from ... 1/n * SUM((inlist(i)-mean)**r)
         n = len(inlist)
         s = 0
         for x in inlist:
-            s = s + (x-mn)**moment
-        return s/float(n)
+            s = s + (x - mn) ** moment
+        return s / float(n)
 
 
-def variation(inlist:List(float))->float:
+def variation(inlist: List[float]) -> float:
     """
 Returns the coefficient of variation, as defined in CRC Standard
 Probability and Statistics, p.6.
 
 Usage:   lvariation(inlist)
 """
-    return 100.0*variability.samplestdev(inlist)/float(central_tendency.mean(inlist))
+    return 100.0 * variability.samplestdev(inlist) / float(central_tendency.mean(inlist))
 
 
-def skew(inlist:List(float))->float:
+def skew(inlist: List[float]) -> float:
     """
 Returns the skewness of a distribution, as defined in Numerical
 Recipies (alternate defn in CRC Standard Probability and Statistics, p.6.)
 
 Usage:   lskew(inlist)
 """
-    return moment(inlist,3)/pow(moment(inlist,2),1.5)
+    return moment(inlist, 3) / pow(moment(inlist, 2), 1.5)
 
 
-def kurtosis(inlist:List(float))->float:
+def kurtosis(inlist: List[float]) -> float:
     """
 Returns the kurtosis of a distribution, as defined in Numerical
 Recipies (alternate defn in CRC Standard Probability and Statistics, p.6.)
 
 Usage:   lkurtosis(inlist)
 """
-    return moment(inlist,4)/pow(moment(inlist,2),2.0)
+    return moment(inlist, 4) / pow(moment(inlist, 2), 2.0)
 
 
-def describe(inlist:List(float))->(int, (float, float), float, float, float, float):
+def describe(inlist: List[float]) -> (int, (float, float), float, float, float, float):
     """
 Returns some descriptive statistics of the passed list (assumed to be 1D).
 
@@ -63,10 +66,9 @@ Usage:   ldescribe(inlist)
 Returns: n, mean, standard deviation, skew, kurtosis
 """
     n = len(inlist)
-    mm = (min(inlist),max(inlist))
+    mm = (min(inlist), max(inlist))
     m = central_tendency.mean(inlist)
     sd = variability.stdev(inlist)
     sk = skew(inlist)
     kurt = kurtosis(inlist)
     return n, mm, m, sd, sk, kurt
-
