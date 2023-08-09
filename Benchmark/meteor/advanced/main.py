f768a84d40f483d9aa7ca5714bb219e166edba08
Vivaan Rajesh <63882442+vivaan2006@users.noreply.github.com>
2023-08-09 16:41:19 -0600

    Update main.py

--- Benchmark/meteor/advanced/main.py
+++ Benchmark/meteor/advanced/main.py
@@ -19,8 +19,10 @@ bg:
 - increased size of board to `20 x 40`
 - time `get_puzzle()`, `get_footprints()` `get_senh()` calls
 """
-
-from Timer import Timer
+from __future__ import annotations
+import __static__
+from __static__ import int64, ArrayI64, CheckedList, CheckedDict
+from typing import List
 
 w, h = 20, 40
 dir_no = 6
@@ -29,27 +31,29 @@ SE = S + (E / 2)
 SW = SE - E
 W, NW, NE = -E, -SE, -SW
 
-def rotate(ido:List(int))->List(float):
-    rd={E: NE, NE: NW, NW: W, W: SW, SW: SE, SE: E}
+
+def rotate(ido: ArrayI64) -> CheckedList[float]:
+    rd = {E: NE, NE: NW, NW: W, W: SW, SW: SE, SE: E}
     return [rd[o] for o in ido]
 
-def flip(ido:List(int))->List(float):
-    fd={E: E, NE: SE, NW: SW, W: W, SW: NW, SE: NE}
+
+def flip(ido: ArrayI64) -> CheckedList[float]:
+    fd = {E: E, NE: SE, NW: SW, W: W, SW: NW, SE: NE}
     return [fd[o] for o in ido]
 
 
-def permute(ido:List(int), r_ido:List(int))->List(float):
+def permute(ido: ArrayI64, r_ido: ArrayI64) -> CheckedList[float]:
     ps = [ido]
     for r in range(dir_no - 1):
         ps.append(rotate(ps[-1]))
-        if ido == r_ido:                 # C2-symmetry
-            ps = ps[0:dir_no//2]
+        if ido == r_ido:  # C2-symmetry
+            ps = ps[0:dir_no // 2]
     for pp in ps[:]:
         ps.append(flip(pp))
     return ps
 
 
-def convert(ido:List(float))->List(float):
+def convert(ido: CheckedList[float]) -> CheckedList[float]:
     '''incremental direction offsets -> "coordinate offsets" '''
     out = [0.0]
     for o in ido:
@@ -57,8 +61,8 @@ def convert(ido:List(float))->List(float):
     return list(set(out))
 
 
-def get_footprints(board:List(float), cti:Dict(float,int), pieces:List(List(List(float))))-> \
-    List(List(List(Set(List(int))))):
+def get_footprints(board: CheckedList[float], cti: CheckedDict[float, int], pieces: CheckedList[CheckedList[CheckedList[float]]]) -> \
+        CheckedList[CheckedList[CheckedList[Set[ArrayI64]]]]:
     fps = [[[] for p in range(len(pieces))] for ci in range(len(board))]
     for c in board:
         for pi, p in enumerate(pieces):
@@ -69,7 +73,7 @@ def get_footprints(board:List(float), cti:Dict(float,int), pieces:List(List(List
     return fps
 
 
-def get_senh(board:List(float), cti:Dict(float,int))->List(Set(List(int))):
+def get_senh(board: CheckedList[float], cti: CheckedDict[float, int]) -> List[Set[ArrayI64]]:
     '''-> south-east neighborhood'''
     se_nh = []
     nh = [E, SW, SE]
@@ -78,11 +82,11 @@ def get_senh(board:List(float), cti:Dict(float,int))->List(Set(List(int))):
     return se_nh
 
 
-def get_puzzle()->(List(float), Dict(float, int), List(List(List(float)))):
-    board = [E*x + S*y + (y%2) for y in range(h) for x in range(w)]
+def get_puzzle() -> (CheckedList[float], CheckedDict(float, int), CheckedList[CheckedList[CheckedList[float]]]):
+    board = [E * x + S * y + (y % 2) for y in range(h) for x in range(w)]
     cti = dict((board[i], i) for i in range(len(board)))
 
-    idos = [[E, E, E, SE],         # incremental direction offsets
+    idos = [[E, E, E, SE],  # incremental direction offsets
             [SE, SW, W, SW],
             [W, W, SW, SE],
             [E, E, SW, SE],
@@ -93,11 +97,13 @@ def get_puzzle()->(List(float), Dict(float, int), List(List(List(float)))):
             [SE, SE, E, SE],
             [E, NW, NW, NW]]
 
-    perms = (permute(p, idos[3]) for p in idos)    # restrict piece 4
+    perms = (permute(p, idos[3]) for p in idos)  # restrict piece 4
     pieces = [[convert(pp) for pp in p] for p in perms]
     return (board, cti, pieces)
 
-def solve(n:int, i_min:int, free:List(int), curr_board:List(int), pieces_left:List(int), solutions:List(int))->Void:
+
+def solve(n: int, i_min: int, free: List(int), curr_board: List(int), pieces_left: List(int),
+          solutions: List(int)) -> None:
     fp_i_cands = fps[i_min]
     for p in pieces_left:
         fp_cands = fp_i_cands[p]
@@ -125,16 +131,15 @@ def solve(n:int, i_min:int, free:List(int), curr_board:List(int), pieces_left:Li
             return
     return
 
+
 SOLVE_ARG = 6000
 
 if __name__ == "__main__":
-    t = Timer()
-    with t:
-      board, cti, pieces = get_puzzle()
-      fps = get_footprints(board, cti, pieces)
-      se_nh = get_senh(board, cti)
-      free = list(range(len(board)))
-      curr_board = [-1] * len(board)
-      pieces_left = list(range(len(pieces)))
-      solutions = []
-      solve(SOLVE_ARG, 0, free, curr_board, pieces_left, solutions)
+    board, cti, pieces = get_puzzle()
+    fps = get_footprints(board, cti, pieces)
+    se_nh = get_senh(board, cti)
+    free = list(range(len(board)))
+    curr_board = [-1] * len(board)
+    pieces_left = list(range(len(pieces)))
+    solutions = []
+    solve(SOLVE_ARG, 0, free, curr_board, pieces_left, solutions)
