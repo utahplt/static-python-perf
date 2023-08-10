3a993fb6200f798020635aeb1fc8e81b62b21dd7
Vivaan Rajesh <63882442+vivaan2006@users.noreply.github.com>
2023-08-09 20:52:07 -0600

    Update test.py

--- Benchmark/pidigits/advanced/test.py
+++ Benchmark/pidigits/advanced/test.py
@@ -1,5 +1,10 @@
 import __static__
 from __static__ import int64, Array
 
-def gen_x(k: int64) -> (int64, int64, int64, int64):
-    return (k, 4 * k + 2, 0, 2 * k + 1)
+def gen_x(k: int64) -> (Array[int64]):
+    test: Array[int64] = Array[int64](4)
+    test[0] = k
+    test[1] = 4 * k + 2
+    test[2] = 0
+    test[3] = 2 * k + 1
+    return (test)
