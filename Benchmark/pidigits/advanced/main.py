4d3ead2edc7dee7c14835a851c786c53b97d1521
Vivaan <vivaanrajesh2006@gmail.com>
2023-08-14 23:22:24 -0600

    Update main.py

--- Benchmark/pidigits/advanced/main.py
+++ Benchmark/pidigits/advanced/main.py
@@ -41,7 +41,11 @@ def compose(a: Tuple[int64, int64, int64, int64], b: Tuple[int64, int64, int64, 
 
 def extract(z: Tuple[int64, int64, int64, int64], j: int64) -> int64:
     q, r, s, t = z
-    return (q * j + r) // (s * j + t)
+    q_int64: int64 = q
+    r_int64: int64 = r
+    s_int64: int64 = s
+    t_int64: int64 = t  
+    return (q_int64 * j + r_int64) // (s_int64 * j + t_int64)
 
 
 def pi_digits(limit: int64) -> List[int64]:
