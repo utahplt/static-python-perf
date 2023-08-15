8bd3ba8e46c72d8f8c522102549e57e43120e20c
Vivaan <vivaanrajesh2006@gmail.com>
2023-08-14 23:44:46 -0600

    Update main.py

--- Benchmark/spectralnorm/advanced/main.py
+++ Benchmark/spectralnorm/advanced/main.py
@@ -6,8 +6,6 @@
 # Dirtily sped up by Simon Descarpentries
 # Concurrency by Jason Stitt
 
-from Timer import Timer
-
 """
 bg:
 - remove `main` function
@@ -15,36 +13,43 @@ bg:
 note, I don't see any concurrency in this code
 """
 
+import __static__
+from __static__ import int64, float64, CheckedList
+from typing import List, Tuple
+from typing import Callable as Function
 
-def eval_A(i:float, j:float) -> float:
+
+def eval_A(i: float, j: float) -> float:
     return 1.0 / ((i + j) * (i + j + 1) // 2 + i + 1)
 
-def eval_times_u(func:Function([Tuple(float, List(float))], float), u:List(float)) -> List(float):
-    return [func((i,u)) for i in range(len(list(u)))]
+
+def eval_times_u(func: Function[[Tuple[float64, List[float64]]], float64], u: List[float64]) -> List[float64]:
+    return [func((i, u)) for i in range(len(CheckedList[u]))]
 
 
-def eval_AtA_times_u(u:List(float))->List(float):
+def eval_AtA_times_u(u: List[float64]) -> List[float64]:
     return eval_times_u(part_At_times_u, eval_times_u(part_A_times_u, u))
 
-def part_A_times_u(i_u:(float, List(float)))->float:
+
+def part_A_times_u(i_u: (float64, List[float64])) -> float64:
     i, u = i_u
     partial_sum = 0
     for j, u_j in enumerate(u):
         partial_sum += eval_A(i, j) * u_j
     return partial_sum
 
-def part_At_times_u(i_u:(float, List(float)))->float:
+
+def part_At_times_u(i_u: (float64, List[float64])) -> float64:
     i, u = i_u
     partial_sum = 0
     for j, u_j in enumerate(u):
         partial_sum += eval_A(j, i) * u_j
     return partial_sum
 
+
 DEFAULT_N = 130
 
 if __name__ == "__main__":
-  t = Timer()
-  with t:
     u = [1] * DEFAULT_N
 
     for dummy in range(10):
@@ -55,4 +60,4 @@ if __name__ == "__main__":
 
     for ue, ve in zip(u, v):
         vBv += ue * ve
-        vv  += ve * ve
+        vv += ve * ve
