fa554a29a8bbd86487db21c8c49aec4eab0bf699
Vivaan <vivaanrajesh2006@gmail.com>
2023-08-14 23:20:37 -0600

    Update main.py

--- Benchmark/pidigits/advanced/main.py
+++ Benchmark/pidigits/advanced/main.py
@@ -1,23 +1,13 @@
-"""Calculating (some of) the digits of pi.  This stresses big integer
-arithmetic."""
-
 """
-bg
-- remove toplevel function, add `ITERATIONS` constant
-- replace `timer` with `Timer`
-- replaced iterators with lists
-- increased NDIGITS to `5000`
+Calculating (some of) the digits of pi. This stresses big integer arithmetic.
 """
 
-# import itertools
-import __static__
 from __static__ import int64, Array
 from typing import List, Tuple
 
-NDIGITS = 5000
+NDIGITS: int = 5000
+ITERATIONS: int = 1000  # Adjust the number of iterations as needed
 
-
-# Adapted from code on http://shootout.alioth.debian.org/
 def gen_x(k: int64) -> Array[int64]:
     test: Array[int64] = Array[int64](4)
     test[0] = k
@@ -35,10 +25,10 @@ def compose(a: Tuple[int64, int64, int64, int64], b: Tuple[int64, int64, int64, 
         as_ * bq + at * bs,
         as_ * br + at * bt
     )
+
 def extract(z: Tuple[int64, int64, int64, int64], j: int64) -> int64:
     q, r, s, t = z
     return (q * j + r) // (s * j + t)
-
 
 def pi_digits(limit: int64) -> List[int64]:
     z = (1, 0, 0, 1)
@@ -57,9 +47,6 @@ def pi_digits(limit: int64) -> List[int64]:
 def calc_ndigits(n: int) -> List[int64]:
     return pi_digits(n)
 
-
 if __name__ == "__main__":
     result = calc_ndigits(NDIGITS)
-    print(result)
-
-
+    print(result) 
