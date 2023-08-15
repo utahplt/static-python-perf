8cfb36170ea6d52fd558a551be113ef171604fd1
Vivaan <vivaanrajesh2006@gmail.com>
2023-08-14 23:19:31 -0600

    Update main.py

--- Benchmark/pidigits/advanced/main.py
+++ Benchmark/pidigits/advanced/main.py
@@ -12,41 +12,39 @@ bg
 # import itertools
 import __static__
 from __static__ import int64, Array
-from typing import List
+from typing import List, Tuple
 
 NDIGITS = 5000
 
 
 # Adapted from code on http://shootout.alioth.debian.org/
-def gen_x(k: int64) -> (Array[int64]):
+def gen_x(k: int64) -> Array[int64]:
     test: Array[int64] = Array[int64](4)
     test[0] = k
     test[1] = 4 * k + 2
     test[2] = 0
     test[3] = 2 * k + 1
-    return (test)
+    return test
 
-
-def compose(a: (int64, int64, int64, int64), b: (int64, int64, int64, int64)) -> (int64, int64, int64, int64):
+def compose(a: Tuple[int64, int64, int64, int64], b: Tuple[int64, int64, int64, int64]) -> Tuple[int64, int64, int64, int64]:
     aq, ar, as_, at = a
     bq, br, bs, bt = b
-    return (aq * bq,
-            aq * br + ar * bt,
-            as_ * bq + at * bs,
-            as_ * br + at * bt)
-
-
-def extract(z: (int64, int64, int64, int64), j: int64) -> int64:
+    return (
+        aq * bq,
+        aq * br + ar * bt,
+        as_ * bq + at * bs,
+        as_ * br + at * bt
+    )
+def extract(z: Tuple[int64, int64, int64, int64], j: int64) -> int64:
     q, r, s, t = z
     return (q * j + r) // (s * j + t)
 
 
-def pi_digits(limit: int64) -> Array[int64]:
+def pi_digits(limit: int64) -> List[int64]:
     z = (1, 0, 0, 1)
     x = 1
-    result = []
-    # test: Array[int64] = Array[int64](4)
-    while (x <= limit):
+    result: List[int64] = []
+    while x <= limit:
         y = extract(z, 3)
         while y != extract(z, 4):
             z = compose(z, gen_x(x))
@@ -56,12 +54,12 @@ def pi_digits(limit: int64) -> Array[int64]:
         result.append(y)
     return result
 
-
-def calc_ndigits(n: int64) -> Array[int64]:
+def calc_ndigits(n: int) -> List[int64]:
     return pi_digits(n)
 
 
 if __name__ == "__main__":
-    calc_ndigits(NDIGITS)
+    result = calc_ndigits(NDIGITS)
+    print(result)
 
 
