9c1f170bcbb85dc5b9f48b5f744ad8ec772af5f3
Vivaan <vivaanrajesh2006@gmail.com>
2023-09-06 20:24:31 -0600

    Update main.py

--- Reliable_perf/main.py
+++ Reliable_perf/main.py
@@ -6,9 +6,10 @@ mean = 10
 std_dev = 1
 sample_size = 8
 
+
 # calcualtes the bootstrap confidence interval by generaating values with replacement from the data, calculating means and percentiles
 # and then calculating the lower and upper bounds of the confidence interval
-def bootstrap_confidence_interval(data, alpha=0.95, num_resamples=10000):
+def bootstrap_confidence_interval(data, alpha=0.05, num_resamples=10000):
     # Generate values with replacement from the data
     resamples = np.random.choice(data, size=(num_resamples, len(data)), replace=True)
     # Calculate the means
@@ -25,8 +26,10 @@ def bootstrap_confidence_interval(data, alpha=0.95, num_resamples=10000):
 
 # this entire process was in the book I mentioned in the meeting.
 """The book is called Design of Observational Studies by Paul R. Rosenbaum."""
+
+
 # the processes are explained step by step as from the book
-def signed_rank_confidence_interval(data, alpha=0.95):
+def signed_rank_confidence_interval(data, alpha=0.05):
     # data in ascending order
     data = np.sort(data)
     # Calculate ranks (mentioned in the book)
@@ -46,49 +49,52 @@ def signed_rank_confidence_interval(data, alpha=0.95):
     upper_bound = sample_mean + (z_alpha * se / np.sqrt(24))
     return lower_bound, upper_bound
 
-# driver function
-def driver(random_number_generator, ci_builder, max_iterations=20):
-    for _ in range(max_iterations):
-        data = random_number_generator()  # init data
-        old_data = data.copy()  # Save the old data
-        # Initialize
-        converged = False
-        iteration = 1
-        while not converged:
-            # Calculate a confidence interval using the provided ci_builder function
-            conf_interval = ci_builder(data)
-            sample_mean = np.mean(data)
-            # 10% interval around the mean
-            sample_mean_10_percent_interval = [sample_mean - 0.1 * sample_mean, sample_mean + 0.1 * sample_mean]
-            # Check if the confidence interval is within the 10% mean interval. Mentioned in meeting?
-            ci_within_10_percent_interval = (
-                    sample_mean_10_percent_interval[0] <= conf_interval[0]
-                    and sample_mean_10_percent_interval[1] >= conf_interval[1]
-            )
-            # If the confidence interval is within the 10% mean interval, data converges
-            if ci_within_10_percent_interval:
-                converged = True
-            else:
-                # If not converged, increment the iteration count and add 8 more values to the old data
-                iteration += 1
-                additional_data = random_number_generator()  # Get 8 more values
-                old_data = np.concatenate((old_data, additional_data))
-                data = old_data  # Use the extended data
 
-        print(f"Iteration {iteration}:")
-        print("Sample:", data)
-        print("Confidence Interval:", conf_interval)
-        print("Sample Mean:", sample_mean)
-        print("10% Mean Interval:", sample_mean_10_percent_interval)
-        print("CI Interval within 10% Interval:", ci_within_10_percent_interval)
-        print("")
+# driver function, not use a for loop, use some conditional
+def driver(random_number_generator, ci_builder, max_iterations=20):
+    data = random_number_generator()  # init data
+    old_data = data.copy()  # Save the old data
+    # Initialize
+    converged = False
+    iteration = 1
+    while (not converged) and (iteration <= max_iterations):
+        # Calculate a confidence interval using the provided ci_builder function
+        conf_interval = ci_builder(data)
+        sample_mean = np.mean(data)
+        # 10% interval around the mean
+        sample_mean_10_percent_interval = [sample_mean - 0.1 * sample_mean, sample_mean + 0.1 * sample_mean]
+        # Check if the confidence interval is within the 10% mean interval. Mentioned in meeting?
+        ci_within_10_percent_interval = (
+                sample_mean_10_percent_interval[0] <= conf_interval[0]
+                and sample_mean_10_percent_interval[1] >= conf_interval[1]
+        )
+        # If the confidence interval is within the 10% mean interval, data converges
+        if ci_within_10_percent_interval:
+            converged = True
+        else:
+            # If not converged, increment the iteration count and add 8 more values to the old data
+            iteration += 1
+            additional_data = random_number_generator()  # Get 8 more values
+            old_data = np.concatenate((old_data, additional_data))
+            data = old_data  # Use the extended data
+
+    print(f"Iteration {iteration}:")
+    print("Sample:", data)
+    print("Confidence Interval:", conf_interval)
+    print("Sample Mean:", sample_mean)
+    print("10% Mean Interval:", sample_mean_10_percent_interval)
+    print("CI Interval within 10% Interval:", ci_within_10_percent_interval)
+    print("")
+
 
 # random number generator functions
 def random_number_generator_normal():
     return np.random.normal(mean, std_dev, sample_size)
 
+
 def random_number_generator_uniform():
     return np.random.uniform(0, 20, sample_size)
+
 
 # Call the driver function for both distributions with different confidence interval builders
 print("Results for Normal Distribution with Bootstrap Confidence Interval:")
