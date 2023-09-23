fe6e58ecb3bb0a5ed27cac9381a40c3dab9422d0
Vivaan Rajesh <63882442+vivaan2006@users.noreply.github.com>
2023-09-23 15:47:48 -0600

    Update driver.py

--- Benchmark/richards/untyped/driver.py
+++ Benchmark/richards/untyped/driver.py
@@ -1,42 +1,56 @@
-import sys
 import os
+import statistics
+from scipy import stats
 
 
-# Execute Python file and collect it's output as a string.
+# Execute Python file and collect its output as a list of runtimes.
 def run_once(file_name):
     command = f"python3 {file_name}"
     stream = os.popen(command)
     output = stream.read()
     return float(output)
 
-# 1. call run many and check for stability by generating runtiem values by calling run many (confirm stability)
-# 2. print out all the runtimes, and the average runtime
-# all this stuff in a new function here
 
 def run_many(file_name, num_iterations):
-    return[run_once(file_name) for i in range(num_iterations)]
+    return [run_once(file_name) for _ in range(num_iterations)]
 
 
+""" Basically, this function checks for stability by generating runtime values by calling run many. Checks if confidence interval is within 10 percent of the sample mean"""
 
 
+def check_stability(file_name, num_iterations):
+    runtimes = run_many(file_name, num_iterations)
 
+    print("Runtimes:")
+    for i, runtime in enumerate(runtimes):
+        print(f"Run {i + 1}: {runtime} seconds")
+
+    mean_runtime = statistics.mean(runtimes)
+
+    std_dev = statistics.stdev(runtimes)
+    confidence_interval = stats.t.interval(0.95, len(runtimes) - 1, loc=mean_runtime,
+                                           scale=std_dev / (len(runtimes) ** 0.5))
+
+    """Stability check to see where the interval is within 10 [percent of the sample mean"""
+    lower_bound, upper_bound = confidence_interval
+    acceptable_range = mean_runtime * 0.1
+
+    if lower_bound <= mean_runtime - acceptable_range or upper_bound >= mean_runtime + acceptable_range:
+        print("Unstable")
+    else:
+        print("Stable")
+
+    # Print the mean and confidence interval
+    print(f"Mean Runtime: {mean_runtime} seconds")
+    print(f"Confidence Interval: {confidence_interval}")
 
 
 if __name__ == "__main__":
-    # num_iterations = 8
-    #
-    # total_runtime = 0
-    #
-    # for i in range(num_iterations):
-    #
-    #     if not success:
-    #         print("Benchmark failed")
-    #         sys.exit(1)
-    #
-    #     total_runtime += runtime
-    #
-    # average_runtime = total_runtime / num_iterations
-    # print(f"Average Runtime over {num_iterations} iterations = {average_runtime} seconds")
+    file_name = "/Users/Vivaan/Desktop/Static\\ Python/static-python-perf/Benchmark/richards/untyped/main.py"
+    num_iterations = 8
+    check_stability(file_name, num_iterations)
 
-    runtime = run_once("/Users/Vivaan/Desktop/Static\\ Python/static-python-perf/Benchmark/richards/untyped/main.py")
-    print(runtime + 1)
+###### Goals for this ########
+# 1. call run many and check for stability by generating runtiem values by calling run many (confirm stability)
+# 2. print out all the runtimes, and the average runtime
+# all this stuff in a new function here
