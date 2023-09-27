d48ed454385ce9f857be63ab092a647e50ff216b
Vivaan <vivaanrajesh2006@gmail.com>
2023-09-27 17:28:20 -0600

    Update driver.py

--- Benchmark/richards/untyped/driver.py
+++ Benchmark/richards/untyped/driver.py
@@ -18,37 +18,46 @@ def run_many(file_name, num_iterations):
 """ Basically, this function checks for stability by generating runtime values by calling run many. Checks if confidence interval is within 10 percent of the sample mean"""
 
 
-def check_stability(file_name, num_iterations):
-    runtimes = run_many(file_name, num_iterations)
+def check_stability(file_name, num_iterations, max_attempts=3):
+    for attempt in range(max_attempts):
+        runtimes = run_many(file_name, num_iterations)
 
-    print("Runtimes:")
-    for i, runtime in enumerate(runtimes):
-        print(f"Run {i + 1}: {runtime} seconds")
+        print(f"Attempt {attempt + 1}:")
+        for i, runtime in enumerate(runtimes):
+            print(f"Run {i + 1}: {runtime} seconds")
 
-    mean_runtime = statistics.mean(runtimes)
+        mean_runtime = statistics.mean(runtimes)
 
-    std_dev = statistics.stdev(runtimes)
-    confidence_interval = stats.t.interval(0.95, len(runtimes) - 1, loc=mean_runtime,
-                                           scale=std_dev / (len(runtimes) ** 0.5))
+        std_dev = statistics.stdev(runtimes)
+        confidence_interval = stats.t.interval(0.95, len(runtimes) - 1, loc=mean_runtime,
+                                               scale=std_dev / (len(runtimes) ** 0.5))
 
-    """Stability check to see where the interval is within 10 [percent of the sample mean"""
-    lower_bound, upper_bound = confidence_interval
-    acceptable_range = mean_runtime * 0.1
+        """Stability check to see where the interval is within 10 percent of the sample mean"""
+        lower_bound, upper_bound = confidence_interval
+        acceptable_range = mean_runtime * 0.1
 
-    if lower_bound <= mean_runtime - acceptable_range or upper_bound >= mean_runtime + acceptable_range:
-        print("Unstable")
-    else:
-        print("Stable")
+        if lower_bound <= mean_runtime - acceptable_range or upper_bound >= mean_runtime + acceptable_range:
+            print("Unstable")
+        else:
+            print("Stable")
 
-    # Print the mean and confidence interval
-    print(f"Mean Runtime: {mean_runtime} seconds")
-    print(f"Confidence Interval: {confidence_interval}")
+        # Print the mean and confidence interval
+        print(f"Mean Runtime: {mean_runtime} seconds")
+        print(f"Confidence Interval: {confidence_interval}")
+
+        if attempt < max_attempts - 1 and (lower_bound <= mean_runtime - acceptable_range or upper_bound >= mean_runtime + acceptable_range):
+            # If it's unstable and there are more attempts remaining, try again.
+            print("Retrying")
+        else:
+            break
 
 
 if __name__ == "__main__":
-    file_name = "/Users/Vivaan/Desktop/Static\\ Python/static-python-perf/Benchmark/richards/untyped/main.py"
+    file_name = "/Users/vivaan/PycharmProjects/Time-Track/static-python-perf/Benchmark/richards/untyped/main.py"
     num_iterations = 8
-    check_stability(file_name, num_iterations)
+    max_attempts = 3  # Maximum number of attempts to stabilize
+    check_stability(file_name, num_iterations, max_attempts)
+
 
 ###### Goals for this ########
 # 1. call run many and check for stability by generating runtiem values by calling run many (confirm stability)
