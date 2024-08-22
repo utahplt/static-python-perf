cdb75d4e0e743ae464b68cbf68e8a58c75264fca
Mrigank Pawagi <mrigankpawagi@gmail.com>
2024-08-22 19:41:36 +0530

    wagtail: add website

---
+++ Benchmark/wagtail/testsite/TestSite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-_goszued$)7@vbue#a#zdew49#n65#qtp8c$x0t8g&d-u$e)ir"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
