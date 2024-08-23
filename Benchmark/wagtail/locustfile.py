267b37eef3a1080b87e753a12e31123b74e55716
Mrigank Pawagi <mrigankpawagi@gmail.com>
2024-08-23 12:54:42 +0530

    wagtail: complete pipeline for editors to create blogs

--- Benchmark/wagtail/locustfile.py
+++ Benchmark/wagtail/locustfile.py
@@ -1,25 +1,22 @@
-import time
+import lorem
+import string
+import random
 import re
-from requests.auth import HTTPBasicAuth
 import logging
 from locust import HttpUser, task, between
 
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
 
 class admin(HttpUser):
-    wait_time = between(1, 5)
-    MAX_USERS_CREATED = 1000
-    fixed_count = 10 # number of admin users to create
+    fixed_count = 0
     num_editors = 0
+    admin_instance = None
+    blogpage_id = None
+    stopped = False
 
-    @task
-    def view_dashboard(self):
-        self.client.get(f"/admin/")
-
-    @task(3)
-    def make_new_editor(self):
-        if admin.num_editors >= admin.MAX_USERS_CREATED:
-            return
-        
+    def make_new_editor(self):        
         with self.client.post(f"/admin/users/new/", data={
             "username": f"ed{admin.num_editors}",
             "password1": "editor1234",
@@ -27,7 +24,7 @@ class admin(HttpUser):
             "email": f"e{admin.num_editors}@su.com",
             "first_name": "ed",
             "last_name": f"e{admin.num_editors}",
-            "groups": 2,
+            "groups": [1, 2],
             "csrfmiddlewaretoken": self.csrftoken,
         }, headers={
             'X-CSRFToken': self.csrftoken,
@@ -37,17 +34,52 @@ class admin(HttpUser):
             response.success()
 
         admin.num_editors += 1
-        editor.fixed_count += 1
+        return admin.num_editors - 1
+
+    def make_blog_index_page(self):
+        with self.client.post("/admin/pages/add/blog/blogindexpage/3/", data={
+            "title": "Blog",
+            "slug": "blog",
+            "intro": """{"blocks":[{"key":"60mvf","text":"This is our blog!","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}""",
+            "comment_notifications": "on",
+            "comments-TOTAL_FORMS": "0",
+            "comments-INITIAL_FORMS": "0",
+            "comments-MIN_NUM_FORMS": "0",
+            "action-publish": "action-publish",
+            "csrfmiddlewaretoken": self.csrftoken,
+        }, headers={
+            'X-CSRFToken': self.csrftoken,
+        }, cookies={
+            "csrftoken": self.csrftoken,
+        }, catch_response=True) as response:
+            response.success()
+
+    def make_home_page(self):
+        with self.client.post("/admin/pages/add/home/homepage/1/", data={
+            "title": "Home",
+            "slug": "home",
+            "comment_notifications": "on",
+            "comments-TOTAL_FORMS": "0",
+            "comments-INITIAL_FORMS": "0",
+            "comments-MIN_NUM_FORMS": "0",
+            "action-publish": "action-publish",
+            "csrfmiddlewaretoken": self.csrftoken,
+        }, headers={
+            'X-CSRFToken': self.csrftoken,
+        }, cookies={
+            "csrftoken": self.csrftoken,
+        }, catch_response=True) as response:
+            response.success()
 
     def on_start(self):
-        # create csrf token and jwt     
+        # create csrf token
         response = self.client.get("/admin/login/")
         self.csrftoken = response.cookies['csrftoken']
 
         self.login()
 
-        # number of users created
-        admin.num_editors = 0
+        self.make_home_page()
+        self.make_blog_index_page()
 
     def login(self):
         # login as admin
@@ -64,14 +96,17 @@ class admin(HttpUser):
     def on_stop(self):
         # get list of user Ids and delete them
         ids_to_delete = set()
-        
+
         i = 1
         while True:
             with self.client.get(f"/admin/users/?p={i}", catch_response=True) as response:
                 response.success()
                 if response.status_code != 200:
                     break
-                ids_to_delete.update({int(x) for x in re.findall(r'data-object-id="(\d+)"', response.text) if int(x) > 1})
+                collected_ids = {int(x) for x in re.findall(r'data-object-id="(\d+)"', response.text) if x and int(x) > 1}
+                if not collected_ids:
+                    break
+                ids_to_delete.update(collected_ids)
                 i += 1
 
         # delete users
@@ -82,35 +117,102 @@ class admin(HttpUser):
         }, cookies={
             "csrftoken": self.csrftoken,
         })
-
-
-class editor(HttpUser):
-    wait_time = between(1, 5)
-    editors_created = 0
-    fixed_count = 0
-
-    def on_start(self):        
-        # create csrf token and jwt     
-        response = self.client.get("/admin/login/")
-        self.csrftoken = response.cookies['csrftoken']
-
-        self.login()
-
-    def login(self):
-        # login as editor
-        logging.info(f"Logging in as editor ed{editor.editors_created}")
-        self.client.post("/admin/login/", data={
-            "username": f"ed{editor.editors_created}",
-            "password": "editor1234",
+        
+        # delete blog
+        response = self.client.post(f"/admin/pages/{admin.get_blogpage_id()}/delete/", data={
             "csrfmiddlewaretoken": self.csrftoken,
         }, headers={
             'X-CSRFToken': self.csrftoken,
         }, cookies={
             "csrftoken": self.csrftoken,
         })
-        
-        editor.editors_created += 1
-    
+
+        admin.stopped = True
+
+    @staticmethod
+    def create_editor(env):
+        if not admin.admin_instance:
+            admin.admin_instance = admin(env)
+            admin.admin_instance.on_start()
+        return admin.admin_instance.make_new_editor()
+
+    @staticmethod
+    def get_blogpage_id():
+        if not admin.blogpage_id:
+            with admin.admin_instance.client.get(f"/admin/pages/3/", catch_response=True) as response:
+                response.success()
+                admin.blogpage_id = re.findall(r'href="/admin/pages/(\d+)/add_subpage', response.text)[-1]
+        return admin.blogpage_id
+
+
+class editor(HttpUser):
+    wait_time = between(5, 10)
+    fixed_count = 1000
+    weight = 1
+    blogs = set() # set of blog slugs
+
+    def on_start(self):
+        # create csrf token
+        response = self.client.get("/admin/login/")
+        self.csrftoken = response.cookies['csrftoken']
+
+        self.id = admin.create_editor(self.environment)
+        self.logged_in = False
+
+        self.login()
+
+    def on_stop(self):
+        if admin.stopped:
+            return
+        admin.admin_instance.on_stop()
+
+    def login(self):
+        # login as editor
+        with self.client.post("/admin/login/", data={
+            "username": f"ed{self.id}",
+            "password": "editor1234",
+            "csrfmiddlewaretoken": self.csrftoken,
+        }, headers={
+            'X-CSRFToken': self.csrftoken,
+        }, cookies={
+            "csrftoken": self.csrftoken,
+        }, catch_response=True) as response:
+            response.success()
+            
+            if response.status_code != 200:
+                return
+
+            self.logged_in = True
+
     @task
     def view_dashboard(self):
-        pass
+        if not self.logged_in: return
+        self.client.get(f"/admin/")
+
+    @task
+    def create_blog_post(self):
+        if not self.logged_in: return
+
+        blog_slug = f"blog{id_generator()}"
+        while blog_slug in editor.blogs:
+            blog_slug = f"blog{id_generator()}"
+
+        with self.client.post(f"/admin/pages/add/blog/blogpage/{admin.get_blogpage_id()}/", data={
+            "title": lorem.sentence(),
+            "slug": blog_slug,
+            "date": "2024-08-22",
+            "intro": lorem.sentence(),
+            "body": """{"blocks":[{"key":"cq08z","text":\"""" + lorem.paragraph() + """\","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}""",
+            "comment_notifications": "on",
+            "comments-TOTAL_FORMS": "0",
+            "comments-INITIAL_FORMS": "0",
+            "comments-MIN_NUM_FORMS": "0",
+            "action-publish": "action-publish",
+            "csrfmiddlewaretoken": self.csrftoken,
+        }, headers={
+            'X-CSRFToken': self.csrftoken,
+        }, cookies={
+            "csrftoken": self.csrftoken,
+        }, catch_response=True) as response:
+            response.success()
+            editor.blogs.add(blog_slug)
